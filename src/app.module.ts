158e5661663ae5c7bc54dce3180c669ec0e4b3e6
Bento <mbento.bauru@gmail.com>
2025-08-11 13:27:09 -0300

    feat: basic typeorm structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OtpModule } from './otp/otp.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [OtpModule],
+  imports: [
+    OtpModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: 'otp',
+      entities: [],
+      synchronize: !!process.env.POSTGRES_SYNCRHONIZE,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
